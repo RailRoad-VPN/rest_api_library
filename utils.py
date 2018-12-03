d4bc6e490b67d0be31009f8d08cbe683bdab3429
dikkini <dikkini@gmail.com>
2018-12-03 12:25:11 +0300

    add logger, fix find method check, add replace comma to dot in unixtime

--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 import calendar
+import logging
 import uuid
 from datetime import datetime
+
+logger = logging.getLogger("rest utils")
 
 
 def check_uuid(suuid: str) -> bool:
@@ -30,24 +33,21 @@ def get_unixtime() -> int:
 
 
 def gen_sec_token() -> str:
-    # print('---------------- gen token')
     token = ""
     ruuid = str(uuid.uuid4())
     ruuid = ruuid.replace("-", "")
     ruuid_len = len(ruuid)
     r4 = str(random_x(1, ruuid_len))
     token += ruuid
+
     unixtime = get_unixtime()
-    # print("Unix time:" + str(unixtime))
-    # print("Unix time len:" + str(len(str(unixtime))))
     unixtime_divided = int(unixtime) / int(r4)
-    # print("Unix time divided:" + str(unixtime_divided))
     unixtime_divided_rounded = "%.10f" % (unixtime_divided)
-    # print("Unix time divided rounded:" + str(unixtime_divided_rounded))
+
     unixtime_divided_len = str(len(str(unixtime_divided_rounded)))
     if len(unixtime_divided_len) == 1:
         unixtime_divided_len = "0" + str(unixtime_divided_len)
-    # print("Unix time rounded len:" + unixtime_divided_len )
+
     left_token = token[:int(r4)]
     center_token = str(unixtime_divided_rounded)
     right_token = token[int(r4):]
@@ -55,19 +55,17 @@ def gen_sec_token() -> str:
     if len(r4) == 1:
         r4 = "0" + str(r4)
     token = str(r4) + str(unixtime_divided_len) + token
-    # print("Random number:" + str(r4))
-    # print("Token: " + str(token))
+
     return token
 
 
 def check_sec_token(token) -> bool:
     if token is None:
         return False
-    # print('---------------- check token')
+
     r4 = token[0:2]
     unixtime_len = token[2:4]
-    # print("Raw number: " + str(r4))
-    # print("Raw unixtime_len: " + str(unixtime_len))
+
     if r4[0] == "0":
         r4 = int(r4[1])
     else:
@@ -76,18 +74,17 @@ def check_sec_token(token) -> bool:
         unixtime_len = int(unixtime_len[1])
     else:
         unixtime_len = int(unixtime_len)
-    # print("Parsed number: " + str(r4))
-    # print("Parsed unixtime_len: " + str(unixtime_len))
 
     token = token[4:]
-    unixtime = token[int(r4):int(unixtime_len) + int(r4)]
-    if str(unixtime).find("."):
+    unixtime = str(token[int(r4):int(unixtime_len) + int(r4)])
+    if unixtime.find(".") != -1:
+        unixtime = float(unixtime)
+    elif unixtime.find(",") != -1:
+        unixtime = unixtime.replace(",", ".")
         unixtime = float(unixtime)
     else:
         unixtime = int(unixtime)
-    # print("Raw unixtime: " + str(unixtime))
     unixtime = round(unixtime * int(r4))
-    # print("Parsed unixtime: " + str(unixtime))
 
     d = datetime.utcfromtimestamp(int(unixtime))
     now_d = datetime.utcnow()
@@ -96,5 +93,5 @@ def check_sec_token(token) -> bool:
     # TODO think about minutes
     delta_minutes = delta_list[0]
     delta_seconds = delta_list[1]
-    print(f"Difference between now date and token date is {delta_minutes} minutes and {delta_seconds} seconds")
+    logger.debug(f"Difference between now date and token date is {delta_minutes} minutes and {delta_seconds} seconds")
     return delta_minutes <= 0 and delta_seconds <= 11
